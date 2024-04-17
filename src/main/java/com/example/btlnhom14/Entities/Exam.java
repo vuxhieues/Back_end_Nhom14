9aa4be8cf987c2b8ab1aa191d1815d5caad5f5f6
vuxhieues <146959891+vuxhieues@users.noreply.github.com>
2024-04-17 14:51:35 +0700

    Update Exam.java

--- src/main/java/com/example/btlnhom14/Entities/Exam.java
+++ src/main/java/com/example/btlnhom14/Entities/Exam.java
@@ -28,24 +28,32 @@ import lombok.NoArgsConstructor;
 @Table(name = "Exam")
 public class Exam {
 
+    // Bảng liệt kê các bài thi.
+    // GenerationType.IDENTITY là id tự động tăng
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long examId;
 
+    // Tên bài thi
     private String examName;
 
+    // Mô tả
     private String description;
 
+    // Loại kì thi vd: cuối kì, giữa kì
     private String examType;
 
 //    @Temporal(TemporalType.TIMESTAMP)
+    // Thời gian bắt đầu
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", timezone = "UTC+7")
     private Date start;
 
 //    @Temporal(TemporalType.TIMESTAMP)
+    // Thời gian kết thúc
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", timezone = "UTC+7")
     private Date end;
 
+    // Xác định mối quan hệ 1-n hoặc 1-n với các bảng tương ứng 
     @OneToMany(mappedBy = "exam", cascade = CascadeType.ALL)
     @JsonIgnore
     private List<Result> Results;
