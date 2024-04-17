75be7b4123ac651ea2b36e3df9e1db504d91b966
vuxhieues <146959891+vuxhieues@users.noreply.github.com>
2024-04-17 15:08:58 +0700

    Update UserDTO.java

--- src/main/java/com/example/btlnhom14/Entities/UserDTO.java
+++ src/main/java/com/example/btlnhom14/Entities/UserDTO.java
@@ -2,6 +2,8 @@ package com.example.btlnhom14.Entities;
 
 import lombok.Data;
 
+// Class này không liên quan đến csdl. Nó giúp mình chỉ lấy ra những thứ mình cần từ class user để phục vụ việc đăng nhập thôi
+// Cái @Data là cái thư viện lombok tự gene ra constructor đấy k liên quan đến csdl.
 @Data
 public class UserDTO {
     private Long userId;
