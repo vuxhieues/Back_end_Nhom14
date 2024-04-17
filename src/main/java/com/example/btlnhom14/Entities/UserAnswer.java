3e8c1e88eed4c43ece657ebaf4bd935660d48d90
vuxhieues <146959891+vuxhieues@users.noreply.github.com>
2024-04-17 15:06:27 +0700

    Update UserAnswer.java

--- src/main/java/com/example/btlnhom14/Entities/UserAnswer.java
+++ src/main/java/com/example/btlnhom14/Entities/UserAnswer.java
@@ -22,31 +22,39 @@ import lombok.NoArgsConstructor;
 @Table(name = "UserAnswer")
 public class UserAnswer {
 
+// bảng liệt kê câu trả lời của USER
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userAnswerId;
 
+    // Câu trả lời USER chọn
     private String selected;
 
+
+    // Xác định quan hệ n-1 với user. Set userId trong bảng user là khóa ngoại
     @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "userId", insertable = false, updatable = false)
     private User user;
 
+    // Xác định quan hệ n-1 với exam. Set examId trong bảng exam là khóa ngoại
     @JsonIgnore
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "examId", insertable = false, updatable = false)
     private Exam exam;
 
+     // Xác định quan hệ n-1 với question. Set questionId trong bảng question là khóa ngoại
     @JsonIgnore
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "questionId", insertable = false, updatable = false)
     private Question question;
 
+    // Các khóa ngoại xác định ở trên
     private Long userId;
     private Long examId;
     private Long questionId;
 
+    //Constructor k liên quan đến csdl
     public UserAnswer(Long userId, Long examId, Long questionId, String selected) {
         this.selected = selected;
         this.userId = userId;
